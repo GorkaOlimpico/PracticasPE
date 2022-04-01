eb8fc3d7184fdb55ae1f623a8518262f92b7eb3b
Gorka Silva <gorka.silva@hotmail.com>
2022-04-01 19:08:02 +0200

    Update CruceCX.java

--- Pr2PE/src/algoritmoGenetico/cruce/CruceCX.java
+++ Pr2PE/src/algoritmoGenetico/cruce/CruceCX.java
@@ -30,21 +30,11 @@ private final String type = "CX";
 			// 1. Inicializo a todo 0s los i3 e i4
 			List<Pair> l3 = new ArrayList<Pair>();
 			List<Pair> l4 = new ArrayList<Pair>();
-			/*
-			Individuo i3 = i1;
-			Individuo i4 = i2;
 			
-			for(int i = 0; i < i3.getGenes().get(j).getLongitud(); i++) { 
-				i3.getGenes().get(j).setAlelo(i, 0);
-			}
-			for(int i = 0; i < i4.getGenes().get(j).getLongitud(); i++) { 
-				i4.getGenes().get(j).setAlelo(i, 0);
-			}
-			*/
 			// 2. Hago un ciclo y paro. Esto con los dos individuos
 
 			int i = 0;
-			while(!contenidoEn(i2.getGenes().get(j).getAlelo(i),l3)) { //si el i2[i] no está en i3
+			while(!contenidoEn(i2.getGenes().get(j).getAlelo(i),l3)) { //si el i2[i] no está en l3
 				for(int k = 0; k < i1.getGenes().get(j).getLongitud(); k++) { // por cada alelo de i1
 					if(i2.getGenes().get(j).getAlelo(i) == i1.getGenes().get(j).getAlelo(k)) { // si ese alelo == i2[i]
 						//i3.getGenes().get(j).setAlelo(k, i1.getGenes().get(j).getAlelo(k)); // lo insertamos en la lista
@@ -69,31 +59,37 @@ private final String type = "CX";
 			
 			// 3. Apunto cuales han sido las posiciones de i1 e i2 que no se han tocado
 			
-			int[] posiciones;
-			for(int t = 0; t <i; t++) {
-				posiciones[t] = t;
+			List<Integer> posiciones = new ArrayList<Integer>();
+			
+			for(int k = 0; k < l3.size(); k++) {
+				boolean aparece = false;
+				for(Pair p: l3) {
+					if(k == (int) p.getFirst()) {
+						aparece = true;
+					}
+				}
+				if(!aparece) {
+					posiciones.add(k);
+				}
 			}
+
+			
+			// 4. Intercambio los alelos de las posiciones correspondientes
+			for(int pos: posiciones) {
+				i1.getGenes().get(j).intercambiarAlelo(pos, i2.getGenes().get(j));
+			}
+			
+			// 5. Hago que el orden de las listas sea el nuevo orden en los individuos
+			
+			
 			for(Pair p: l3) {
-				if()
+				i1.getGenes().get(j).setAlelo((int) p.getFirst(), p.getSecond());
 			}
 			
-			for(int k = 0; k < i3.getGenes().get(j).getLongitud(); k++) {
-				if(i3.getGenes().get(j).getAlelo(k) == (Object) 0) {
-					i3.getGenes().get(j).setAlelo(k, i2.getGenes().get(j).getAlelo(k));
-				}
+			for(Pair p: l4) {
+				i2.getGenes().get(j).setAlelo((int) p.getFirst(), p.getSecond());
 			}
-			
-			for(int k = 0; k < i4.getGenes().get(j).getLongitud(); k++) {
-				if(i4.getGenes().get(j).getAlelo(k) == (Object) 0) {
-					i4.getGenes().get(j).setAlelo(k, i1.getGenes().get(j).getAlelo(k));
-				}
-			}
-			
-			// 4. i1 ahora es i3, i2 ahora es i4
-			
-			i1 = i3;
-			i2 = i4;
-			//TODO Ojo esto puede dar problemas por los punteros y tal
+			System.out.println("final");
 		}
 	}
 	
