2e5a7c310b313a89fb349f5aef0b36eef66d1b70
RO8ER7O <robplaza@ucm.es>
2022-04-22 20:46:32 +0200

    Generar hoja

--- Pr3PE/src/arbol/Hoja.java
+++ Pr3PE/src/arbol/Hoja.java
@@ -13,16 +13,14 @@ public abstract class Hoja extends Arbol {
 		double prob = rand.nextDouble();
 		if(prob < 1/6)
 			return new HojaA0(profundidad, padre);
-		if(prob < 1/6)
-			return new HojaA0(profundidad, padre);
-		if(prob < 1/6)
-			return new HojaA0(profundidad, padre);
-		if(prob < 1/6)
-			return new HojaA0(profundidad, padre);
-		if(prob < 1/6)
-			return new HojaA0(profundidad, padre);
-		if(prob < 1/6)
-			return new HojaA0(profundidad, padre);
-		return null;
+		if(prob < 2/6)
+			return new HojaA1(profundidad, padre);
+		if(prob < 3/6)
+			return new HojaD0(profundidad, padre);
+		if(prob < 4/6)
+			return new HojaD1(profundidad, padre);
+		if(prob < 5/6)
+			return new HojaD2(profundidad, padre);
+		return new HojaD3(profundidad, padre);
 	}
 }
