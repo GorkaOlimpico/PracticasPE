3d6c23b0c3967c4e0ca43bc351cd49743ab79ba9
Gorka Silva <gorka.silva@hotmail.com>
2022-05-04 09:56:29 +0200

    update GE

--- Pr3PE/src/algoritmoGenetico/AlgoritmoGenetico.java
+++ Pr3PE/src/algoritmoGenetico/AlgoritmoGenetico.java
@@ -91,13 +91,13 @@ public class AlgoritmoGenetico {
 			
 			sacarElites(elites);
 			
-			System.out.println("Seleccion");
+		//	System.out.println("Seleccion");
 			seleccion.select(poblacion, poblacionAux);
-			System.out.println("Cruce");
+			//System.out.println("Cruce");
 			cruce.cruzar(poblacion, prob_cruce / 100);
-			System.out.println("Mutacion");
+			//System.out.println("Mutacion");
 			mutacion.mutar(poblacion, prob_mutacion / 100);
-			System.out.println("Bloating"); 			
+			//System.out.println("Bloating"); 			
 			poblacion[0].bloating(poblacion);
 			
 			ordenarPoblacion();
